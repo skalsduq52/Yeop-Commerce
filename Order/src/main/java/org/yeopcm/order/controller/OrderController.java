14a19103464e8a6775af22813e57952786a916eb
skalsduq52 <skalsduq@naver.com>
2025-02-24 15:20:29 +0900

    Order 서비스 점검

--- Order/src/main/java/org/yeopcm/order/controller/OrderController.java
+++ Order/src/main/java/org/yeopcm/order/controller/OrderController.java
@@ -1,4 +1,4 @@
-package controller;
+package org.yeopcm.order.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
